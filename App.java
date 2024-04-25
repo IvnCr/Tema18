288b72e679a5fef7869337eb81cbdc208b24a654
IvnCr <icr0015@alu.medac.es>
2024-04-25 15:57:22 +0200

    commit develop

--- App.java
+++ App.java
@@ -2,5 +2,6 @@ public class App{
     public static void main(String[] args) {
         System.out.println("Hola mundo");
         System.out.println("develop");
+        System.out.println("commit develop");
     }
 }
