adf9451c28f09e800b504ef1801658d096cf5720
IvnCr <icr0015@alu.medac.es>
2024-04-25 16:00:10 +0200

    commit 2

--- App.java
+++ App.java
@@ -2,5 +2,6 @@ public class App{
     public static void main(String[] args) {
         System.out.println("Hola mundo");
         System.out.println("commit feature");
+        System.out.println("commit feature 2");
     }
 }
